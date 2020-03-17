b39809a179f3a25528053c83e79b39f107580ecd
Komei Shimamura <komei.t.f@gmail.com>
2020-03-17 19:25:39 +0000

    Check aoj test command

--- cmd/test.go
+++ cmd/test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"fmt"
 	"github.com/fatih/color"
+	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 	"github.com/travelist/aoj-cli/cmd/conf"
 	"io"
@@ -46,8 +47,13 @@ var testCommand = func(command *cobra.Command, args []string) {
 		os.Exit(1)
 	}
 
+	if len(testIns) == 0 {
+
+	}
+
+	fmt.Printf("Found %d test cases\n", len(testIns))
 	for index, _ := range testIns {
-		fmt.Printf("Running test #%d\n", index)
+		fmt.Printf("Running test set #%d\n", index)
 
 		if e := executeBeforeEach(); e != nil {
 			fmt.Printf("%v\n", e)
@@ -68,15 +74,17 @@ var testCommand = func(command *cobra.Command, args []string) {
 		}
 
 		if success {
-			fmt.Printf("%s: test case #{%d}\n", color.GreenString("PASS"), index)
+			fmt.Printf("%s: test set #%d\n", color.GreenString("PASS"), index)
 		} else {
-			fmt.Printf("%v: test case #{%d}\n", color.RedString("FAIL"), index)
+			fmt.Printf("%v: test set #%d\n", color.RedString("FAIL"), index)
 			fmt.Printf("[Input]\n")
-			_ = showFileBody(testIns[index])
+			in, _ := getFileBody(testIns[index])
+			fmt.Printf("%s", in)
 			fmt.Printf("[Expected]\n")
-			_ = showFileBody(testOuts[index])
+			out, _ := getFileBody(testOuts[index])
+			fmt.Printf("%s", color.RedString(out))
 			fmt.Printf("[Actual]\n")
-			fmt.Printf(string(output))
+			fmt.Printf(color.RedString(string(output)))
 		}
 
 		if e := executeAfterEach(); e != nil {
@@ -95,14 +103,24 @@ var testCommand = func(command *cobra.Command, args []string) {
 func executeBeforeAll() error {
 	c := conf.GetTestBeforeAll()
 	if len(c) == 0 {
+		fmt.Printf("[%s] %s\n", color.GreenString("Before All"), color.BlueString("pass"))
 		return nil
 	}
 
 	a := strings.Split(c, " ")
-	e := exec.Command(a[0], a[1:]...).Run()
+	fmt.Printf("[%s] %s\n", color.GreenString("Before All"), color.BlueString(c))
 
+	var out, err bytes.Buffer
+	command := exec.Command(a[0], a[1:]...)
+	command.Stdout = &out
+	command.Stderr = &err
+
+	e := command.Run()
+
+	fmt.Printf(out.String())
+	fmt.Printf(color.RedString(err.String()))
 	if e != nil {
-		return e
+		return errors.Wrap(e, "failed to execute [test].before_all command")
 	}
 
 	return nil
@@ -114,6 +132,7 @@ func executeBeforeEach() error {
 		return nil
 	}
 
+	fmt.Printf("[%s] %s\n", color.GreenString("Before Each"), color.BlueString(c))
 	a := strings.Split(c, " ")
 	e := exec.Command(a[0], a[1:]...).Run()
 
@@ -125,6 +144,7 @@ func executeBeforeEach() error {
 }
 
 func executeCommand(command []string, inputFilePath string) ([]byte, error) {
+	fmt.Printf("[%s] %s\n", color.GreenString("Test"), color.BlueString(strings.Join(command, " ")))
 	in, e := os.Open(inputFilePath)
 	if e != nil {
 		return []byte{}, fmt.Errorf("cannot open %s, %v", inputFilePath, e)
@@ -152,20 +172,19 @@ func checkOutput(expectedOutputFilePath string, output []byte) (bool, error) {
 	return bytes.Equal(exp, output), nil
 }
 
-func showFileBody(filePath string) error {
+func getFileBody(filePath string) (string, error) {
 	in, e := os.Open(filePath)
 	if e != nil {
-		return fmt.Errorf("cannot open %s, %v", filePath, e)
+		return "", fmt.Errorf("cannot open %s, %v", filePath, e)
 	}
 	defer in.Close()
 
 	body, e := ioutil.ReadAll(in)
 	if e != nil {
-		return fmt.Errorf("cannot read %s, %v", filePath, e)
+		return "", fmt.Errorf("cannot read %s, %v", filePath, e)
 	}
 
-	fmt.Printf("%s", string(body))
-	return nil
+	return string(body), nil
 }
 
 func executeAfterEach() error {
@@ -173,6 +192,8 @@ func executeAfterEach() error {
 	if len(c) == 0 {
 		return nil
 	}
+
+	fmt.Printf("[%s] %s\n", color.GreenString("After Each"), color.BlueString(c))
 
 	a := strings.Split(c, " ")
 	e := exec.Command(a[0], a[1:]...).Run()
@@ -245,6 +266,7 @@ func executeAfterAll() error {
 	if len(c) == 0 {
 		return nil
 	}
+	fmt.Printf("[%s] %s\n", color.GreenString("After Each"), color.BlueString(c))
 
 	a := strings.Split(c, " ")
 	e := exec.Command(a[0], a[1:]...).Run()
